fd0ea62a5691e977622eea0b043dde84ebe4f444
bettyfeyissa <bettyfeyissa@gmail.com>
2022-11-17 16:13:24 -0600

    created a variables and constructor for Patient class

--- src/main/java/Patient.java
+++ src/main/java/Patient.java
@@ -7,4 +7,15 @@ public class Patient {
     String emergencyContactInfo;
     int ssn;
 
+
+    //create a constructor
+    public Patient(String name, int insuranceId, String description, String address, String emergencyContactInfo, int ssn) {
+        this.name = name;
+        this.insuranceId = insuranceId;
+        this.description = description;
+        this.address = address;
+        this.emergencyContactInfo = emergencyContactInfo;
+        this.ssn = ssn;
+
+    }
 }
