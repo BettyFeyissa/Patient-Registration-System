136cd6578bdbb451081ef4ba643455d561f41a97
bettyfeyissa <bettyfeyissa@gmail.com>
2022-11-17 15:48:44 -0600

    intial commit

---
+++ src/main/java/Patient.java
@@ -0,0 +1,10 @@
+public class Patient {
+    //Create class attributes
+    String name;
+    int insuranceId;
+    String description;
+    String address;
+    String emergencyContactInfo;
+    int ssn;
+
+}
