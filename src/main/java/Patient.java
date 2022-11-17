648f09610c9aafa2390709093c48a7bf79f2030c
bettyfeyissa <bettyfeyissa@gmail.com>
2022-11-17 16:23:12 -0600

    Created a getter & setter methods

--- src/main/java/Patient.java
+++ src/main/java/Patient.java
@@ -18,4 +18,52 @@ public class Patient {
         this.ssn = ssn;
 
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getInsuranceId() {
+        return insuranceId;
+    }
+
+    public void setInsuranceId(int insuranceId) {
+        this.insuranceId = insuranceId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmergencyContactInfo() {
+        return emergencyContactInfo;
+    }
+
+    public void setEmergencyContactInfo(String emergencyContactInfo) {
+        this.emergencyContactInfo = emergencyContactInfo;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
 }
