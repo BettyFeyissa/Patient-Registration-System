2241b89c67d0326104a9f77dca6f64ec4030e6f0
bettyfeyissa <bettyfeyissa@gmail.com>
2022-11-17 22:15:56 -0600

    Added print option for the user to select

--- src/main/java/Appointment.java
+++ src/main/java/Appointment.java
@@ -7,8 +7,16 @@ public class Appointment {
     static Scanner input = new Scanner(System.in);
     // Creating a new instance of PatientRegistrationSystem
     static PatientRegistrationSystem pat = new PatientRegistrationSystem();
+    // Sorting the users input
+    static String Option = " ";
+
+    static void PresentMenu() {
+        //Print option for the user to select
+
+        String optionMessage = "\nType C to Create new Appointments \n" + "Type R to read your Appointments \n" + "Type U to update your Appointments\n" + "Type E to Exit \n";
+        System.out.println(optionMessage);
 
     }
 
-
+}
 
